1dbf5127dd18dc1d635dff83e60282fc3428e3d3
stepanv1 <stepanv1@gmail.com>
2019-05-05 19:10:21 -0400

    debug

--- DW.R
+++ DW.R
@@ -1,23 +1,25 @@
-# Simulate CDF of doubly singularbeta ensemble
+# Simulate CDF of lambda_max of doubly singular beta ensemble
 # and compare to the exact formula
 library(rootWishart)
 library(rWishart)
 library(corpcor)
 # set parameteres of doubly singular beta ensemble (DSB)
-p=500
+p=120
 m=100
 q=6
-n=5000
+n=500
 #generate singular wishart matrices with Sigma = I_p
 A=rSingularWishart(n, m, diag(1, p))
 B=rSingularWishart(n, q, diag(1, p))
 #compute \lambda_max for DSB samples
 lmax<-rep(0,n)
+rnks<-rep(0,n)
 for(i in 1:n){
-  svdA <- svd(A[,,i])
+  svdA <- eigen(A[,,i])
   rankVr <- corpcor::rank.condition(A[,,i])$rank
-  eigVecA <- svdA$v[, 1:rankVr]
-  eigValAInv <- 1/svdA$d[1:rankVr]
+  rnks[i] <- rankVr
+  eigVecA <- svdA$vectors[, 1:rankVr]
+  eigValAInv <- sqrt(1/svdA$values[1:rankVr])
   Xp <- eigVecA %*% diag(eigValAInv)
   C <- crossprod(Xp, B[,,i] %*% Xp)
   svdC <- corpcor::fast.svd(C)
@@ -32,8 +34,11 @@ lval <- sort(lmax/(1+lmax))
 #plot empirical cdf:
 DW<-doubleWishart(lval, p=q, n=m, m = p-m+q,  type = "multiple")
 # plot on the original scale:
-plot(ecdf(lmax))
+plot(ecdf(lmax), pch='.')
 points(lmax, DW, col='red', pch='.')
+
+
+
 
 
 
